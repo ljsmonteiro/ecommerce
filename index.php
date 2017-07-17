bc9b49cfd04ee448dcafc571fb9652d22f94c819
Lucio Monteiro <lucio.suruagy@gmail.com>
2017-07-17 17:50:38 -0300

    page admin / TPL Admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,10 +15,14 @@ $app->get('/', function() {
 	$page = new Page();
 
 	$page->setTPL("index");
-	//$sql = new Hcode\DB\Sql();
-	//$sql = new Sql();
-	//$results = $sql->select("SELECT * FROM tb_users");
-	//echo json_encode($results);
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTPL("index");
 
 });
 
